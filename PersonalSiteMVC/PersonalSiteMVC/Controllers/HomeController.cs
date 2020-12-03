fc86ac073bdabffc912a08a89d99cb9e46fddc20
David See <engr12b10@hotmail.com>
2020-12-03 01:18:54 -0600

    initial commit

---
+++ PersonalSiteMVC/PersonalSiteMVC/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonalSiteMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Resume()
+        {           
+
+            return View();
+        }
+
+        public ActionResult Portfolio()
+        {           
+
+            return View();
+        }
+
+        public ActionResult Classmates()
+        {           
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {            
+
+            return View();
+        }
+    }
+}
